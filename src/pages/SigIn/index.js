8122c971375def0b330a0f391f1a1613bb0c5e8d
Lenon <lenonsdp@hotmail.com>
2020-07-26 16:29:11 -0300

    Criação page signIn e SignUp e estilo no statusBar

--- src/pages/SigIn/index.js
+++ src/pages/SigIn/index.js
@@ -5,13 +5,36 @@ import logo from '../../assets/logo.png'
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Background from '../../components/background';
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
 
-export default function SignIn() {
-  return(
-    <Background>
-      <Container>
-        <Image source={logo} />
-      </Container>
-    </Background>
-  );
+export default function SignIn({ navigation }) {
+	return(
+		<Background>
+			<Container>
+				<Image source={logo} />
+
+				<Form>
+					<FormInput 
+						icon="mail-outline"
+						keyboardType="email-address"
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Digite seu e-mail"
+					/>
+					
+					<FormInput 
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Digite sua senha"
+					/>
+
+					<SubmitButton onPress={() => {}} >Acessar</SubmitButton>
+				</Form>
+
+				<SignLink onPress={() => navigation.navigate('SignUp')}>
+					<SignLinkText>Criar conta gratuita</SignLinkText>
+				</SignLink>
+			</Container>
+		</Background>
+	);
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
+import logo from '../../assets/logo.png'
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 import Background from '../../components/background';
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
 
-export default function SignUp() {
-  return(
-    <Background>
-        <Text>SignUp</Text>
-    </Background>
-  );
+export default function SignIn({ navigation }) {
+	return(
+		<Background>
+			<Container>
+				<Image source={logo} />
+
+				<Form>
+
+          <FormInput 
+						icon="person-outline"
+						keyboardType="email-address"
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Digite seu nome completo"
+					/>
+
+					<FormInput 
+						icon="mail-outline"
+						keyboardType="email-address"
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Digite seu e-mail"
+					/>
+					
+					<FormInput 
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Digite sua senha"
+					/>
+
+					<SubmitButton onPress={() => {}} >Acessar</SubmitButton>
+				</Form>
+
+				<SignLink onPress={() => navigation.navigate('SignUp')}>
+					<SignLinkText>Criar conta gratuita</SignLinkText>
+				</SignLink>
+			</Container>
+		</Background>
+	);
 }
