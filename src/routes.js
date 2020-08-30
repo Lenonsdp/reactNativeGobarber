2c7cef19f0b59b15a77f7120e4f784b5b0504772
Lenon <lenonsdp@hotmail.com>
2020-08-30 14:32:07 -0300

    Criação da roda de agendar;

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
-import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import SignIn from '../src/pages/SigIn';
 import SignUp from '../src/pages/SignUp';
+import SelectProvider from '../src/pages/New/SelectProvider';
+import SelectDateTime from '../src/pages/New/SelectDateTime';
+import Confirm from '../src/pages/New/Confirm';
+
 import Dashboard from '../src/pages/Dashboard';
 import Profile from '../src/pages/Profile';
 
@@ -14,6 +21,28 @@ export default (isSigned = false) => createAppContainer(
 		}),
 		App: createBottomTabNavigator({
 			Dashboard,
+			New: {
+				screen: createStackNavigator({
+					SelectProvider,
+					SelectDateTime,
+					Confirm
+				}, {
+					defaultNavigationOptions: {
+						headerTransparent: true,
+						headerTintColor: '#FFF',
+						headerLeftContainerStyle: {
+							marginLeft: 20,
+						}
+					}
+				}),
+				navigationOptions: {
+					tabBarVisible: false,
+					tabBarLabel: 'Agendar',
+					tabBarIcon: (
+						<Icon name="add-circle-outline" size={20} color="rgba(255, 255, 255, 0.6)" />
+					)
+				}
+			},
 			Profile
 		}, {
 			tabBarOptions: {
